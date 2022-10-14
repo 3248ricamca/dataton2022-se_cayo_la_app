eef31ed3c59da7aa757c8cebacc79c8bea1e16d4
Daniel Ricardo Amaya Carpeta <daniel.amaya@challenger.lcl>
2022-10-14 14:02:08 -0500

    Se agrega el script correcto

--- src/script.py
+++ src/script.py
@@ -1,9 +1,7 @@
 ## este script contiene una prueba para richi
 
-param1 = 25
-param2 = 50
-
-def run_id(param1, param2):param1 + param2
-    print(rund_id(param1,param2))	
+def run_id(param1,param2): 
+            suma = param1 + param2
+            return suma	
     # esta funcion debe sumar el parametro uno con el parametro dos y retornar la sumatoria 
-    pass
+	RTA: run_id(28,109)
