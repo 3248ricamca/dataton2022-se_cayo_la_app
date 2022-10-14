2932dcb2ce6e77c29058f9e0dd8031b5011afd17
Alejandro Cano <alejandrocanomn@gmail.com>
2022-10-14 14:35:20 -0500

    correccion y buenas practicas ejericio

--- src/script.py
+++ src/script.py
@@ -1,7 +1,16 @@
 ## este script contiene una prueba para richi
 
-def run_id(param1,param2): 
-            suma = param1 + param2
-            return suma	
-    # esta funcion debe sumar el parametro uno con el parametro dos y retornar la sumatoria 
-	RTA: run_id(28,109)
+def run_id(param1,param2):
+    """
+    Esta funcion debe sumar el parametro uno con el parametro dos y retornar la sumatoria
+    
+    Inputs:
+        param1: int: parametro 1
+        param2: int: parametro 2
+    
+    Outputs: int:
+        Sumatoria del parametro 1 y 2
+        
+    """
+    suma = param1 + param2
+    return suma
