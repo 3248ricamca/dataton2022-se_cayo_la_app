04118bab8d81333101965cf1847174b03f58e2e9
Daniel Ricardo Amaya Carpeta <daniel.amaya@challenger.lcl>
2022-10-14 12:44:54 -0500

    Please enter the commit message for your changes. Lines starting

--- src/script.py
+++ src/script.py
@@ -1,5 +1,9 @@
 ## este script contiene una prueba para richi
 
-def run_id(param1, param2):
+param1 = 25
+param2 = 50
+
+def run_id(param1, param2):param1 + param2
+    print(rund_id(param1,param2))	
     # esta funcion debe sumar el parametro uno con el parametro dos y retornar la sumatoria 
     pass
