ffacaf5aaae2ae88f2f8ba57802f3ddbfbb4f78f
Alejandro Cano <alejandrocanomn@gmail.com>
2022-10-30 14:34:42 -0500

    script de creacion de siglas

---
+++ src/recomendador/create_json.py
@@ -0,0 +1,5 @@
+
+from recomendador.utils.participacion import crear_json_aux
+
+if __name__ == '__main__':
+    crear_json_aux(abspath = 'src/data/archivos_auxiliares/diccionario_siglas.json')
--- src/script.py
+++
@@ -1,6 +0,0 @@
-
-from posixpath import abspath
-from recomendador.utils import crear_json_aux
-
-if __name__ == '__main__':
-    crear_json_aux(abspath = 'data/')
