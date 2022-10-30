4aa182232487eeb7d4c47c623f795b35a8003ade
Alejandro Cano <alejandrocanomn@gmail.com>
2022-10-30 14:38:24 -0500

    acalaracion de ejecucion sobre script de json

--- src/recomendador/create_json.py
+++ src/recomendador/create_json.py
@@ -1,3 +1,4 @@
+# No es necesario ejecutar este script ya que dentro del repositorio ya existe el archivo json diccionario_siglas.json
 
 from recomendador.utils.participacion import crear_json_aux
 
